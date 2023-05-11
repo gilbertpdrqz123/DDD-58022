f85a6b731d51ab2a55023943e10ecd5f0665007d
Gilbert Padriquez <99242029+gilbertpdrqz123@users.noreply.github.com>
2023-05-11 11:58:46 +0800

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,5 @@
+SELECT * FROM abc_computer.students_info;
+SELECT * FROM abc_computer.students_info WHERE city = 'Cavite';
+SELECT * FROM abc_computer.students_info WHERE sem_grade = '80';
+SELECT * FROM abc_computer.students_info WHERE gender = 'F';
+SELECT * FROM abc_computer.students_info WHERE entry_age = '17';
